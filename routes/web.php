97e2bf3f22b9a2b2f993c85aefc09e8dc8cf6062
DionBeatus <beatusdion@gmail.com>
2026-05-04 12:34:04 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
-
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
